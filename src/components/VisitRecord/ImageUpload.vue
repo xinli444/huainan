469f060891df5ed42f5efb9c836eec4033bad86a
lixin <lixin@qyzt.net.cn>
2024-04-20 17:52:45 +0800

    参观记录

---
+++ src/components/VisitRecord/ImageUpload.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { useVModel } from '@vueuse/core'
+import { ElUpload, ElIcon, ElImage } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+
+const props = defineProps({
+  modelValue: String
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const modelValue = useVModel(props, 'modelValue', emit)
+
+const triggerRef = ref()
+
+const fileList = ref([])
+
+watch(
+  modelValue,
+  () => {
+    fileList.value = modelValue.value ? [{ url: modelValue.value }] : []
+  },
+  { immediate: true }
+)
+
+function handleChange(uploadFile) {
+  const url = URL.createObjectURL(uploadFile.raw)
+  modelValue.value = url
+}
+
+function handleImageLoad() {
+  URL.revokeObjectURL(modelValue.value)
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <el-image
+      v-if="fileList.length"
+      :src="fileList[0].url"
+      class="w-[148px] h-[148px]"
+      fit="scale-down"
+      @load="handleImageLoad"
+    />
+    <div v-else class="el-upload el-upload--picture-card" @click="triggerRef.click()">
+      <el-icon><Plus /></el-icon>
+    </div>
+    <el-upload
+      :file-list="fileList"
+      :show-file-list="false"
+      :auto-upload="false"
+      accept=".jpg,.png"
+      action="#"
+      class="mt-[8px]"
+      :on-change="handleChange"
+    >
+      <div ref="triggerRef">
+        <slot></slot>
+      </div>
+    </el-upload>
+  </div>
+</template>
--- src/views/VisitRecord.vue
+++ src/views/VisitRecord.vue
@@ -1,5 +1,165 @@
-<script setup></script>
+<script setup>
+import { ref, shallowRef, watch } from 'vue'
+import { ElButton, ElRadioGroup, ElRadio } from 'element-plus'
+import { fabric } from 'fabric'
+import { saveAs } from 'file-saver'
+import ImageUpload from '@/components/VisitRecord/ImageUpload.vue'
+
+const peopleImageUrl = ref()
+const sceneImgUrl = ref()
+
+const canvasElRef = ref()
+const canvasRef = shallowRef()
+const peopleImgRef = shallowRef()
+const sceneImgRef = shallowRef()
+
+const filterValue = ref('0')
+const filterList = ref([
+  { label: '原图', value: '0' },
+  { label: '灰度', value: '1' },
+  { label: '底片', value: '2' },
+  { label: '黑白', value: '3' },
+  { label: '怀旧', value: '4' },
+  { label: '模糊', value: '5' },
+  { label: '老照片', value: '6' },
+  { label: '混合', value: '7' },
+  { label: '调和', value: '8' },
+  { label: '棕仙', value: '9' },
+  { label: '彩色胶片', value: '10' },
+  { label: '马赛克', value: '11' },
+  { label: '宝丽来', value: '12' },
+  { label: '彩色', value: '13' }
+])
+
+watch(canvasElRef, () => {
+  canvasRef.value = new fabric.Canvas(canvasElRef.value)
+  peopleImageUrl.value = '/people.png'
+  sceneImgUrl.value = '/scene.jpg'
+})
+
+function setPeopleImg() {
+  peopleImgRef.value && canvasRef.value?.remove(peopleImgRef.value)
+
+  canvasRef.value.remove()
+  fabric.Image.fromURL(peopleImageUrl.value, function (img) {
+    img.scale(0.5).set({
+      left: 100,
+      top: 100
+    })
+    setImgFilters(img)
+    peopleImgRef.value = img
+    canvasRef.value?.insertAt(img, 1)
+  })
+}
+watch(peopleImageUrl, setPeopleImg)
+
+function setSceneImg() {
+  sceneImgRef.value && canvasRef.value?.remove(sceneImgRef.value)
+
+  fabric.Image.fromURL(sceneImgUrl.value, function (img) {
+    img.scale(0.5).set({
+      left: 0,
+      top: 0
+    })
+    setImgFilters(img)
+    img.set('selectable', false)
+    sceneImgRef.value = img
+    canvasRef.value?.insertAt(img, 0)
+  })
+}
+watch(sceneImgUrl, setSceneImg)
+
+watch(filterValue, () => {
+  setPeopleImg()
+  setSceneImg()
+})
+
+function setImgFilters(image) {
+  switch (filterValue.value) {
+    case '0':
+      break //原图
+    case '1':
+      image.filters.push(new fabric.Image.filters.Grayscale()) //灰度
+      break
+    case '2':
+      image.filters.push(new fabric.Image.filters.Invert()) //底片
+      break
+    case '3':
+      image.filters.push(new fabric.Image.filters.BlackWhite()) // 纯黑白
+      break
+    case '4':
+      image.filters.push(new fabric.Image.filters.Sepia()) // 復古 怀旧
+      break
+    case '5':
+      image.filters.push(
+        new fabric.Image.filters.Blur({
+          blur: 0.5
+        })
+      ) // 模糊
+      break
+    case '6':
+      image.filters.push(new fabric.Image.filters.Vintage()) //老照片
+      break
+    case '7':
+      image.filters.push(new fabric.Image.filters.BlackWhite()) // 纯黑白
+      image.filters.push(new fabric.Image.filters.Sepia()) // 復古   混合
+      break
+    case '8':
+      image.filters.push(new fabric.Image.filters.BlendColor()) //盲目的
+      break
+    case '9':
+      image.filters.push(new fabric.Image.filters.Brownie()) //棕仙
+      break
+    case '10':
+      image.filters.push(new fabric.Image.filters.Kodachrome()) //彩色胶片
+      break
+    case '11':
+      image.filters.push(new fabric.Image.filters.Pixelate()) //马赛克
+      break
+    case '12':
+      image.filters.push(new fabric.Image.filters.Polaroid()) //宝丽来
+      break
+    case '13':
+      image.filters.push(new fabric.Image.filters.Technicolor()) //彩色
+      break
+  }
+  image.applyFilters()
+}
+
+function exportPicture() {
+  const dataUrl = canvasRef.value.toDataURL()
+  saveAs(dataUrl, `${new Date().getTime()}.png`)
+}
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="flex justify-center items-center space-x-[16px]">
+      <div>
+        <ImageUpload v-model="peopleImageUrl">
+          <el-button type="primary">
+            <span>上传人像</span>
+          </el-button>
+        </ImageUpload>
+        <ImageUpload v-model="sceneImgUrl">
+          <el-button type="primary">
+            <span>选择风景</span>
+          </el-button>
+        </ImageUpload>
+      </div>
+      <div>
+        <canvas ref="canvasElRef" width="960" height="540"></canvas>
+        <div class="mt-[8px]">
+          <el-radio-group v-model="filterValue">
+            <template v-for="{ label, value } in filterList" :key="value">
+              <el-radio :label="label" :value="value" />
+            </template>
+          </el-radio-group>
+        </div>
+        <div class="text-right">
+          <el-button type="primary" @click="exportPicture">导出照片</el-button>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
