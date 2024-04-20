e613072b55e5f1310941e4c0031c85af63f6c93f
lixin <lixin@qyzt.net.cn>
2024-04-20 18:04:37 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,39 @@
+/* eslint-env node */
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  esbuild: {
+    drop: isProd ? ['console', 'debugger'] : []
+  },
+  build: {
+    reportCompressedSize: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: '[hash].js',
+        entryFileNames: '[hash].js',
+        assetFileNames: '[hash].[ext]',
+        manualChunks(id) {
+          // if (id.includes('element-plus')) {
+          //   return 'element-plus'
+          // }
+          if (id.includes('fabric')) {
+            return 'fabric'
+          }
+        }
+      }
     }
   }
 })
